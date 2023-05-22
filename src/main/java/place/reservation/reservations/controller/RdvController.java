0fafd978c82b28267615ad5b7f3aeb3466e18585
MadMan <mr.mad2k@gmail.com>
2023-05-22 20:14:53 +0200

    Add RDV controller

--- src/main/java/place/reservation/reservations/controller/RdvController.java
+++ src/main/java/place/reservation/reservations/controller/RdvController.java
@@ -1,10 +1,28 @@
 package place.reservation.reservations.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import place.reservation.reservations.entity.Rdv;
+import place.reservation.reservations.service.RdvService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/rdvs")
 public class RdvController {
 
+    private final RdvService rdvService;
+
+    public RdvController(RdvService rdvServiceInput) {
+        this.rdvService = rdvServiceInput;
+    }
+
+    @GetMapping(value = {"/", ""})
+    public String showRdvs(Model model) {
+        List<Rdv> allRdvs = rdvService.getAllRdvs();
+        model.addAttribute("rdvs", allRdvs);
+        return "rdvs";
+    }
 }
--- src/main/java/place/reservation/reservations/service/RdvService.java
+++ src/main/java/place/reservation/reservations/service/RdvService.java
@@ -1,7 +1,11 @@
 package place.reservation.reservations.service;
 
 import org.springframework.stereotype.Service;
+import place.reservation.reservations.entity.Rdv;
 import place.reservation.reservations.repository.RdvRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RdvService {
@@ -10,5 +14,17 @@ public class RdvService {
 
     public RdvService(RdvRepository rdvRepositoryInput) {
         this.rdvRepository = rdvRepositoryInput;
+    }
+
+    public List<Rdv> getAllRdvs() {
+        Iterable<Rdv> rdvsIterable = rdvRepository.findAll();
+        List<Rdv> rdvsList = new ArrayList<>();
+        for (Rdv rdv : rdvsIterable) {
+            if (rdv != null) {
+                rdvsList.add(rdv);
+            }
+        }
+
+        return rdvsList;
     }
 }
