5f477b5ce67b482b86575108653899907a1ceb15
MadMan <mr.mad2k@gmail.com>
2023-05-24 12:45:20 +0200

    add Procedure DTO

---
+++ src/main/java/place/reservation/reservations/dto/ProcedureDto.java
@@ -0,0 +1,15 @@
+package place.reservation.reservations.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProcedureDto {
+
+    private String name;
+
+    private int duration;
+}
